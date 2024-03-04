3402e3ad0b5340c63d302b104be89bc3caf2b2dd
debrunbaix <sasha.deroubaix.dev@proton.me>
2024-03-04 23:51:16 +0100

    add analyse/checksec_cmd function and assemble obj

--- app/analyse.py
+++ app/analyse.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import json
 
 FILENAME = sys.argv[1]
 BINARY_NAME = (FILENAME.split('/'))[-1]
@@ -10,18 +11,25 @@ CHECKSEC_CMD = ['checksec', '--output=json', '--file=' + FILENAME]
 
 def filter_file_output(output):
     output_list = output.split(', ')
-    file_cmd_info = [
-        ((output_list[0]).split(' '))[0],
-        ((output_list[0]).split(' '))[1],
-        output_list[3],
-        output_list[7]
-    ]
-    print(file_cmd_info)
+    file_cmd_info = {
+        "format": ((output_list[0]).split(' '))[0],
+        "bit": 32 if ((output_list[0]).split(' '))[1] == "32-bit" else 64,
+        "linked": output_list[3],
+        "stripped": "no" if "not" in output_list[7] else "yes"
+    }
+    return file_cmd_info
+
+def filter_checksec_output(output):
+    output_list = json.loads(output)[FILENAME]
+    return output_list
 
 def main():
     file_cmd_output = subprocess.check_output(FILE_CMD, universal_newlines=True)
-    filter_file_output(file_cmd_output)
+    obj_binary_info = filter_file_output(file_cmd_output)
 
     checksec_cmd_output = subprocess.check_output(CHECKSEC_CMD, universal_newlines=True)
-    print(checksec_cmd_output)
+    obj_binary_info.update(filter_checksec_output(checksec_cmd_output))
+
+    for x in obj_binary_info:
+        print(f'{x} : {obj_binary_info[x]}')
 main()
